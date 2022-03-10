bc17655e94fd43953c49f9620c555774c1a248f7
Hilton Shumway <hillshum@gmail.com>
2022-03-09 23:55:28 -0600

    Improve dashcam clock accuracy
    Don't calcuate the new time string until after the HTTP call for connection is made.
    That call takes a few seconds, which means the string provided is a few seconds
    stale if calcuated first

--- src/yicam.py
+++ src/yicam.py
@@ -9,12 +9,12 @@ DISCONNECT_PARAMS = OrderedDict(custom=1, cmd=8002)
 SET_CLOCK_PARAMS = OrderedDict(custom=1, cmd=3034)
 
 def update_time_calls():
+    connect = requests.get(PATH, params=CONNECT_PARAMS)
+    print(connect.content)
     now = datetime.datetime.now()
     date_time = now.strftime("%Y-%m-%-d_%H:%M:%S")
     print(date_time)
     SET_CLOCK_PARAMS['str'] =  date_time
-    connect = requests.get(PATH, params=CONNECT_PARAMS)
-    print(connect.content)
     sett = requests.get(PATH, params=SET_CLOCK_PARAMS)
     print(sett.content)
     disconnect = requests.get(PATH, DISCONNECT_PARAMS)
