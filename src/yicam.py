e8ab3fb9f6da3d146d63930dd7d3a88b615b64e7
Hilton Shumway <hillshum@gmail.com>
2022-06-25 12:07:56 -0500

    Fast timeout on dashcam update

--- src/yicam.py
+++ src/yicam.py
@@ -9,7 +9,7 @@ DISCONNECT_PARAMS = OrderedDict(custom=1, cmd=8002)
 SET_CLOCK_PARAMS = OrderedDict(custom=1, cmd=3034)
 
 def update_time_calls():
-    connect = requests.get(PATH, params=CONNECT_PARAMS)
+    connect = requests.get(PATH, params=CONNECT_PARAMS, timeout=5)
     print(connect.content)
     now = datetime.datetime.now()
     date_time = now.strftime("%Y-%m-%-d_%H:%M:%S")
